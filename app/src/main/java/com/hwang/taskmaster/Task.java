c4984735a8f007225e26587dbe994483742dc732
dhwang21 <hwang.derrick@gmail.com>
2019-01-30 23:22:16 -0800

    quick refactor

--- app/src/main/java/com/hwang/taskmaster/Task.java
+++ app/src/main/java/com/hwang/taskmaster/Task.java
@@ -1,12 +1,10 @@
 package com.hwang.taskmaster;
 
-import android.text.TextUtils;
 
 import java.io.Serializable;
 
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 
 
@@ -21,57 +19,29 @@ public class Task implements Serializable {
   @TypeConverters(StateConverter.class)
   public State taskState;
 
-  public enum State {
-    AVAILABLE("AVAILABLE"), ASSIGNED("ASSIGNED"), ACCEPTED("ACCEPTED"), FINISHED("FINISHED");
-    protected final String value;
-
-    State(String value) {
-      this.value = value;
-    }
-  }
-
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
-  public String getDescription() {
-    return description;
-  }
-  public void setDescription(String description) {
-    this.description = description;
-  }
-  public String getFinishBy() {
-    return finishBy;
-  }
-  public void setFinishBy(String finishBy) {
-    this.finishBy = finishBy;
-  }
-
   public Task(){}
 
-  public String toString(){
-    return "Task name: " + title + " Description: " + description +  " Finish by: " + finishBy;
-  }
+  public enum State {AVAILABLE("AVAILABLE"), ASSIGNED("ASSIGNED"), ACCEPTED("ACCEPTED"), FINISHED("FINISHED");
+    protected final String value;
+    State(String value) {
+      this.value = value;}}
 
+  public String getTitle() { return title; }
 
+  public void setTitle(String title) { this.title = title; }
 
+  public String getDescription() { return description; }
 
+  public void setDescription(String description) { this.description = description; }
 
+  public String getFinishBy() { return finishBy; }
 
+  public void setFinishBy(String finishBy) { this.finishBy = finishBy; }
 
-  public void setTaskState(State state){
-      this.taskState = state;
-    }
+  public State getState(){ return taskState;}
 
-    public State getState(){
-      return taskState;
-    }
+  public void setTaskState(State state){ this.taskState = state;}
 
-
-
-
-
+  public String toString(){ return "Task name: " + title + " Description: " + description +  " Finish by: " + finishBy;}
 
 }
