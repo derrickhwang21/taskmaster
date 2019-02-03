d529de6ab44d7bb7f7941e2293c5c32f5c30dd09
dhwang21 <hwang.derrick@gmail.com>
2019-02-02 18:44:20 -0800

    checking out branch

--- app/src/main/java/com/hwang/taskmaster/Activity/MainActivity.java
+++ app/src/main/java/com/hwang/taskmaster/Activity/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.hwang.taskmaster.R;
@@ -17,6 +19,7 @@ import com.hwang.taskmaster.R;
 import java.util.Arrays;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -27,12 +30,15 @@ public class MainActivity extends AppCompatActivity {
    * TODO refactor this into a file to reference and incremement in the future if necessary
    */
   private static final int RC_SIGN_IN = 3742;
+  private FirebaseAuth mFirebaseAuth;
+  private FirebaseUser mFirebaseUser;
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
 
   }
 
@@ -49,6 +55,17 @@ public class MainActivity extends AppCompatActivity {
                     .setAvailableProviders(providers)
                     .build(),
             RC_SIGN_IN);
+  }
+
+  public void onSignOutButtonPressed(View v){
+    AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener(new OnCompleteListener<Void>() {
+              public void onComplete(@NonNull Task<Void> task) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                finish();
+              }
+            });
   }
 
   @Override
